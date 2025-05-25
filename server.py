de31dc56974a38239fcba3ade59100968b0afaf9
jonathan.torrez <jonathan.torrez@icloud.com>
2025-05-25 19:35:21 +0200

    Update server.py

--- server.py
+++ server.py
@@ -4,7 +4,3 @@ app = Flask(__name__)
 
 app.secret_key = 'supersecretkey'
 app.permanent_session_lifetime = 99999999
-
-@app.route("/")
-def home():
-    return "¡La app Flask está funcionando en Render!"

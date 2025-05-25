2d508755ad4516e17b5b785acc5930ac4b95ca13
jonathan.torrez <jonathan.torrez@icloud.com>
2025-05-25 19:32:38 +0200

    Update main.py
    cambio puerto para render

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from server import app
 from routes import auth, companies, companies_admin, users_admin
 
 if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
     app.run(debug=True, use_reloader=False)
